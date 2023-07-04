94d326d0618990649e1c17de85f360b58fb510fb
kaan ceylan <kaanceyllan@gmail.com>
2023-07-04 15:17:32 +0300

    UI Text

--- Realm Rush/Assets/Bank/Bank.cs
+++ Realm Rush/Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,27 +10,39 @@ public class Bank : MonoBehaviour
 
     [SerializeField] int currentBalance;
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     public int CurrentBalance { get { return currentBalance; } }
 
      void Awake()
     {
         currentBalance = StartingBalance;
+        UpdateDisplay();
     }
 
     public void Deposite(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if(currentBalance < 0)
+        if (currentBalance < 0)
         {
-
+            ReloadScene();
         }
-    } void ReloadScene()
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold:" + currentBalance;
+    }
+
+    void ReloadScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
